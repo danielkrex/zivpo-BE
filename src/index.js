09b4a47a7678a34dc59676721cff0fde7545f2a6
danielkrex <danielkrex@gmail.com>
2026-04-21 23:03:56 +0200

    helthz

--- src/index.js
+++ src/index.js
@@ -17,6 +17,7 @@ app.use(cors({
 app.use(express.json())
 
 app.get('/health', (req, res) => res.json({ status: 'ok' }))
+app.get('/healthz', (req, res) => res.send('OK'))
 
 app.use('/api/auth', authRoutes)
 app.use('/api/admin', adminRoutes)
